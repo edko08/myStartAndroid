080d51e04a64cba82c197950c183466eac302622
Edko <Edko@ua.fm>
2018-09-30 09:00:53 +0300

    Secand new commit

--- app/src/main/java/com/ed/startandroid/EventCover.java
+++ app/src/main/java/com/ed/startandroid/EventCover.java
@@ -1,33 +1,23 @@
 package com.ed.startandroid;
 
-import android.app.Activity;
-import android.app.SearchManager;
-import android.content.Context;
-import android.net.sip.SipSession;
-import android.support.v4.os.CancellationSignal;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
 
-public class EventCover extends Activity implements View.OnClickListener {
+public class EventCover implements View.OnClickListener {
+    private final MainActivity main;
 
-    private static final String Clicked = "ghghg";
-    int idView;
-
-    public EventCover() { }
+     EventCover(MainActivity main) {
+        this.main = main;
+    }
 
     @Override
     public void onClick(View view) {
-        String context =  "ghghgh";
         switch (view.getId()) {
             case R.id.button_ok:
-              //  Toast.makeText(context, Clicked, Toast.LENGTH_LONG).show();
+                main.onClickOk();
                 break;
             case R.id.button_cancel:
-
-              //  Toast.makeText(context, Clicked, Toast.LENGTH_LONG).show();
+                main.onClickCancel();
                 break;
-
         }
     }
 }
--- app/src/main/java/com/ed/startandroid/MainActivity.java
+++ app/src/main/java/com/ed/startandroid/MainActivity.java
@@ -4,9 +4,12 @@ import android.app.LoaderManager;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +22,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setElements();
+        setPress();
 
-       // buttonOk.setOnClickListener(new EventCover());
+
+    }
+
+    private void setElements(){
+        buttonOk = (Button) findViewById(R.id.button_ok);
+        buttonCancel = (Button) findViewById(R.id.button_cancel);
+        userName = (TextView) findViewById(R.id.editTextUserName);
+        password = (TextView) findViewById(R.id.editTextPassword);
+    }
+
+    private void setPress(){
+        buttonOk.setOnClickListener(new EventCover(this));
+        buttonCancel.setOnClickListener(new EventCover(this));
+    }
+
+    public void onClickOk(){
+        String st_userName = userName.getText().toString();
+        if (TextUtils.isEmpty(st_userName)) {
+            Toast.makeText(getApplicationContext(), "Не заполнено имя пользователя", Toast.LENGTH_LONG).show();
+            return;
+        }
+        String st_password = password.getText().toString();
+        if (TextUtils.isEmpty(st_password)) {
+            Toast.makeText(getApplicationContext(), "Не заполнен пароль", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+    }
+
+    public void onClickCancel() {
+        Toast.makeText(getApplicationContext(), "Cancel", Toast.LENGTH_LONG).show();
     }
 }
